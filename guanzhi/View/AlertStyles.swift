1e7f93b7a9b4c68d22c5558d86b43575ee19ba3a
ZaptainZ <captainzi@qq.com>
2024-02-26 13:43:24 +0800

    测试

---
+++ guanzhi/View/AlertStyles.swift
@@ -0,0 +1,18 @@
+//
+//  AlertStyles.swift
+//  guanzhi
+//
+//  Created by 晨光 訾 on 2024/2/20.
+//
+
+import SwiftUI
+
+struct AlertStyles: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlertStyles()
+}
